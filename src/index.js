7325cd51725121d29e6a7320fd3d180749ca3860
adaptedbee <adaptedbee@yandex.ru>
2019-08-12 01:20:04 +0300

    Создаём рендер-методы для заголовка и списка

--- src/index.js
+++ src/index.js
@@ -7,12 +7,19 @@ import products from './products.json';
 function App() {
   const productsLimit = 3;
 
+  function renderHeadline() {
+    return <h1 className="headline">Список товаров</h1>;
+  }
+  function renderProducts() {
+    return <ul className="products">
+      {products.slice(0, productsLimit).map((item, index) => <li key={item.id}>{item.name}</li>)}
+    </ul>;
+  }
+
   return (
     <React.Fragment>
-      <h1 className="headline">Список товаров</h1>
-      <ul className="products">
-      {products.slice(0, productsLimit).map((item, index) => <li key={item.id}>{item.name}</li>)}
-      </ul>
+      {renderHeadline()}
+      {renderProducts()}
     </React.Fragment>
   );
 }
