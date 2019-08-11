be5589affc6c47dad083098a99f8a603b2394425
adaptedbee <adaptedbee@yandex.ru>
2019-08-12 02:08:57 +0300

    Подключаем компонент карточки товара

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,12 +2,29 @@ import React from 'react';
 
 import products from '../../products.json';
 import './Products.css';
+import ProductItem from 'csssr-school-product-card';
+
+const ratingComponent = ({ isFilled }) => {
+  return <div className={isFilled ? "starFill" : ""} />;
+};
 
 const Products = props => {
   const productsLimit = 3;
   
   return <ul className="products">
-    {products.slice(0, productsLimit).map((item, index) => <li key={item.id}>{item.name}</li>)}
+    {products.slice(0, productsLimit).map((item, index) => 
+      <ProductItem
+        key={item.id}
+        isInStock={item.isInStock}
+        img={item.img}
+        title={item.title}
+        price={item.price}
+        subPriceContent={item.subPriceContent}
+        maxRating={item.maxRating}
+        rating={item.rating}
+        ratingComponent={ratingComponent}
+      />
+    )}
   </ul>;
 }
 
