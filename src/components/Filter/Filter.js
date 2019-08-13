244dea36f636b18fd47052b72be6ad3b0831055e
adaptedbee <adaptedbee@yandex.ru>
2019-08-14 01:04:26 +0300

    Фильтр: обновляем state

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -12,33 +12,62 @@ class Filter extends React.Component {
       minPrice: Math.min(...productsPrices),
       maxPrice: Math.max(...productsPrices)
     };
+
+    this.minPriceInput = React.createRef();
+    this.maxPriceInput = React.createRef();
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      minPrice: this.parsePriceInput(this.minPriceInput.current.value),
+      maxPrice: this.parsePriceInput(this.maxPriceInput.current.value),
+    });
+  }
+
+  parsePriceInput(value) {
+    const numberValue = Number(value);
+    if (numberValue <= 0) {
+      return null;
+    } else {
+      return numberValue;
+    }
   }
 
   render() {
-    return <form className="filter-form">
-      <h3 className="filter-form__headline">Цена</h3>
-      <div className="filter-form__price-range-wrapper">
-        <label 
-          className="filter-form__label" 
-          htmlFor="from-price">от</label>
-        <input 
-          className="filter-form__text-input filter-form__text-input--price" 
-          id="from-price"
-          type="text"
-          defaultValue={this.state.minPrice} />
-        <label 
-          className="filter-form__label" 
-          htmlFor="to-price">до</label>
-        <input 
-          className="filter-form__text-input filter-form__text-input--price" 
-          id="to-price" 
-          type="text"
-          defaultValue={this.state.maxPrice} />
-      </div>
-      <button 
-        className="filter-form__button"
-        type="button">Применить</button>
-    </form>;
+    return (
+      <form 
+        className="filter-form"
+        onSubmit={this.handleSubmit}>
+        <h3 className="filter-form__headline">Цена</h3>
+        <div className="filter-form__price-range-wrapper">
+          <label 
+            className="filter-form__label" 
+            htmlFor="from-price">от</label>
+          <input 
+            className="filter-form__text-input filter-form__text-input--price" 
+            id="from-price"
+            type="text"
+            defaultValue={this.state.minPrice}
+            ref={this.minPriceInput} />
+          <label 
+            className="filter-form__label" 
+            htmlFor="to-price">до</label>
+          <input 
+            className="filter-form__text-input filter-form__text-input--price" 
+            id="to-price" 
+            type="text"
+            defaultValue={this.state.maxPrice}
+            ref={this.maxPriceInput} />
+        </div>
+        <button 
+          className="filter-form__button"
+          type="submit">Применить</button>
+
+        <p>{this.state.minPrice}</p>
+        <p>{this.state.maxPrice}</p>
+      </form>
+    );
   }
 }
 
