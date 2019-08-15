41075dfc817c428f968e820a2dd8baebe6359c03
adaptedbee <adaptedbee@yandex.ru>
2019-08-15 22:33:08 +0300

    Правки: рефакторинг Headline

--- src/components/Headline/Headline.js
+++ src/components/Headline/Headline.js
@@ -5,37 +5,16 @@ import './Headline.css';
 
 class Headline extends React.Component {
   render() {
-    if (this.props.size === 1) {
-      return (
-        <h1 className="headline">{this.props.text}</h1>
-      );
-    } else if (this.props.size === 2) {
-      return (
-        <h2 className="headline">{this.props.text}</h2>
-      );
-    } else if (this.props.size === 3) {
-      return (
-        <h3 className="headline">{this.props.text}</h3>
-      );
-    } else if (this.props.size === 4) {
-      return (
-        <h4 className="headline">{this.props.text}</h4>
-      );
-    } else if (this.props.size === 5) {
-      return (
-        <h5 className="headline">{this.props.text}</h5>
-      );
-    } else {
-      return (
-        <h6 className="headline">{this.props.text}</h6>
-      );
-    }
-    
+    const Tag = `h${this.props.size > 6 ? 6 : this.props.size}`;
+
+    return (
+      <Tag className="headline">{this.props.text}</Tag>
+    );
   }
 }
 
 Headline.propTypes = {
-  size: PropTypes.number,
+  size: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
   text: PropTypes.string
 };
 
