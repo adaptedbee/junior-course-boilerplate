2c3747dbd4c476238375ea2fb2c7e04e5c1da3ae
adaptedbee <adaptedbee@yandex.ru>
2019-08-16 23:23:53 +0300

    Правки: обработка значений в фильтре

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { logger } from 'csssr-school-utils';
-import toInt from 'csssr-school-utils/src/toInt';
 
 import Headline from '../Headline/Headline.js';
 
@@ -22,10 +21,22 @@ class Filter extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const minPrice = toInt(this.minPriceInput.current.value);
-    const maxPrice = toInt(this.maxPriceInput.current.value);
+    const parsedMinPrice = this.parsePriceInput(this.minPriceInput.current.value);
+    const parsedMaxPrice = this.parsePriceInput(this.maxPriceInput.current.value);
+    const minPrice = parsedMinPrice !== null ? parsedMinPrice : this.props.minPrice;
+    const maxPrice = parsedMaxPrice !== null ? parsedMaxPrice : this.props.maxPrice;
     this.props.updatePriceFilter(minPrice, maxPrice);
   }
+
+  parsePriceInput = (value) => {
+    const string = value.replace(/\D/g, '');
+    const integer = parseInt(string, 10);
+    if (string.length === 0 || isNaN(integer) || integer < 0) {
+      return null;
+    }
+
+    return integer;
+  };
 
   render() {
     return (
